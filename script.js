be42242aefaceaa4f054342c3175d69c781ae64e
Aradhya <120170628+aradhya-9032@users.noreply.github.com>
2023-05-22 19:43:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 
 //your code here
-	const bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Aerosmith', 'Rolling Stones', 'Queen', 'Anthrax', 'Black Sabbath'];
 
-function sortBands(bands) {
-  return name.replace(/^(a |an |the )/i, '');
+let bandNames = ['The Beatles', 'Red Hot Chili Peppers', 'The Rolling Stones', 'Metallica', 'Aerosmith'];
+
+// Function to remove articles from the band name
+function removeArticles(name) {
+  return name.replace(/^(a|an|the)\s+/i, '');
 }
-let sortedBandNames = bandNames.sort((a, b) => {
+
+// Sort the band names in lexicographic order while excluding articles
+bandNames.sort(function(a, b) {
   return removeArticles(a).localeCompare(removeArticles(b));
 });
-const ulElement = document.getElementById('band');
-sortedBandNames.forEach(name => {
-  const liElement = document.createElement('li');
-  liElement.textContent = name;
-  ulElement.appendChild(liElement);
-});
+
+// Get the <ul> element by its ID
+let bandList = document.getElementById('band');
+
+// Loop through the sorted band names and create <li> elements
+for (let i = 0; i < bandNames.length; i++) {
+  let listItem = document.createElement('li');
+  listItem.textContent = bandNames[i];
+  bandList.appendChild(listItem);
+}
+
+
+
+
+// 	const bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Aerosmith', 'Rolling Stones', 'Queen', 'Anthrax', 'Black Sabbath'];
+
+// function sortBands(bands) {
+//   return name.replace(/^(a |an |the )/i, '');
+// }
+// let sortedBandNames = bandNames.sort((a, b) => {
+//   return removeArticles(a).localeCompare(removeArticles(b));
+// });
+// const ulElement = document.getElementById('band');
+// sortedBandNames.forEach(name => {
+//   const liElement = document.createElement('li');
+//   liElement.textContent = name;
+//   ulElement.appendChild(liElement);
+// });
 
 
 
